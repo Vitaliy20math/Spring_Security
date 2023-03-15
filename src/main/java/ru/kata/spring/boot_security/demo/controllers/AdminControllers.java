4c88922fad9dc4a3a2a5f9a277099e03cd024ee3
Vitaliy <akhkyamiev@inbox.ru>
2023-03-15 14:25:37 +0400

    clean

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminControllers.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminControllers.java
@@ -41,18 +41,6 @@ public class AdminControllers {
         return "userById";
     }
 
-    /*@GetMapping("/new")
-    public String newPerson(Model model) {
-        model.addAttribute("user", new User());
-        return "form_for_new_user";
-    }*/
-
-  /*  @PostMapping()
-    public String create(@ModelAttribute("user") User user){
-        userService.saveUser(user);
-        return "redirect:/users";
-    }*/
-
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") Long id) {
         model.addAttribute("user", userService.findUserById(id));
@@ -75,58 +63,5 @@ public class AdminControllers {
         return "redirect:/admin";
     }
 
-   /* @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("users", userService.allUsers());
-        return "listUsers";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String edit(Model model, @PathVariable("id") Long id) {
-        model.addAttribute("user", userService.findUserById(id));
-        return "edit";
-    }
-
-    @PatchMapping("admin/")
-    public String update(@ModelAttribute("user") User user) {
-        userService.update(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/{id}")
-    public String getById(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("userId", userService.findUserById(id));
-        return "userById";
-    }
-
-    @GetMapping("/new")
-    public String newPerson(Model model) {
-        model.addAttribute("user", new User());
-        return "form_for_new_user";
-    }
-
-    @PostMapping()
-    public String create(@ModelAttribute("user") User user){
-        userService.saveUser(user);
-        return "redirect:/users";
-    }
-
-    *//*@GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") Long id) {
-        model.addAttribute("user", userService.findUserById(id));
-        return "edit";
-    }*//*
-
-    *//*@PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User user) {
-        userService.update(user);
-        return "redirect:/users/{id}";
-    }*//*
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
-        return "redirect:/users";
-    }*/
 
 }
